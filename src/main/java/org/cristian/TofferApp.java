8baab7b6c732833f1213e003f31775f95a9ea739
roblescristian4722 <roblescristian4722@gmail.com>
2025-10-22 19:06:51 -0600

    Starting point

---
+++ src/main/java/org/cristian/TofferApp.java
@@ -0,0 +1,11 @@
+package org.cristian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofferApp {
+	public static void main(String[] args) {
+        SpringApplication.run(TofferApp.class, args);
+	}
+}
